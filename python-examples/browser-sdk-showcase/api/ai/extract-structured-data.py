686ae69c697352f29675f07b11676d651ef71282
ilaiwi <ahmad@intunedhq.com>
2025-12-24 12:35:03 +0200

    ok

--- python-examples/browser-sdk-showcase/api/ai/extract-structured-data.py
+++ python-examples/browser-sdk-showcase/api/ai/extract-structured-data.py
@@ -33,7 +33,7 @@ async def automation(page: Page, params: Params | None = None, **_kwargs):
         data_schema=Book,  # Pass Pydantic model directly, or use a normal json schema dictionary too.
         prompt="Extract book details from this page",
         enable_cache=False,  # To enable cache, you must run in Intuned context (IDE/CLI) and save the project, with the correct API credentials.
-        max_retires=3,
+        max_retries=3,
     )
     print(f"Found product: {product['name']} - {product['price']}")
 
